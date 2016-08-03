b1a7cc6ce93ef47d129d8582aad90ba28a061953
Hannah Swift <h.graceswift@gmail.com>
2016-08-03 09:18:01 -0500

    Visiting /todo-group shows a list, User can navigate to add new todo from main list

--- tests/acceptance/todo-group-test.js
+++ tests/acceptance/todo-group-test.js
@@ -10,3 +10,24 @@ test('visiting /todo-group', function(assert) {
     assert.equal(currentURL(), '/todo-group');
   });
 });
+
+test('visiting /todo-group shows a list', function(assert) {
+  server.createList('todo-group', 2);
+  visit('/todo-group');
+
+  andThen(function() {
+    findWithAssert('.todo-list');
+    assert.equal(findWithAssert('.todo-list__item').length, 2,
+      'There should be something, something, something.');
+  });
+});
+
+test('user can navigate to add new todo from main list', function(assert) {
+  visit('/todo-group');
+  click('.new-link');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/todo-group/new');
+    assert.equal(findWithAssert('.page-title').text().trim(), 'New Todo');
+  });
+});
